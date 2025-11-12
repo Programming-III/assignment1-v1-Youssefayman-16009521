bb17e843e268b684eab8570b8e94fdf0e78eacd5
Youssefayman-16009521 <youssefdorgham13@gmail.com>
2025-11-12 15:19:25 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,22 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
+#include <iostream>
 #include <string>
 using namespace std;
+Class Animal{
+    private:
+string name;
+int age;
+bool isHungry;
+    public:
+        Animal();
+        Animal(string name,int age,bool isHungry);
+        ~Animal();
+        void display();
+        void feed();
 
-//define animal class here 
+};
 
 
 
-#endif
+#endif // ANIMAL_H_INCLUDED
