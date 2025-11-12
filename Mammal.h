0430d8338d306c841405f9fea86d7132dae3d4db
Youssefayman-16009521 <youssefdorgham13@gmail.com>
2025-11-12 15:20:58 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,17 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
-
+#ifndef MAMMAL_H_INCLUDED
+#define MAMMAL_H_INCLUDED
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
+Class Mammal:public Animal{
+    private:
+string furColor;
+public :
+    Mammal();
+    Mammal(string furColor);
+    ~Mammal();
+};
 
-//define mammal class here
 
-
-
-
-
-
-#endif
+#endif // MAMMAL_H_INCLUDED
