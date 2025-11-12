aa8c32fbadd82bdc9cca96519d8c7dcd08d31478
Youssefayman-16009521 <youssefdorgham13@gmail.com>
2025-11-12 15:22:25 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,19 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#ifndef VISITOR_H_INCLUDED
+#define VISITOR_H_INCLUDED
+#include <iostream>
 #include <string>
 using namespace std;
+class Visitor{
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+    Visitor();
+    Visitor(string visitorName, int ticketsBought);
+    ~Visitor();
+    void displayInfo();
+    int getTicketsBought();
+};
 
-//define visitor class here
 
-
-
-
-
-
-
-#endif
+#endif // VISITOR_H_INCLUDED
